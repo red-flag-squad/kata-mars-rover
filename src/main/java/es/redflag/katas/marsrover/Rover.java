0306e59eeacb081c88a0763a3613a57687319e7a
Ricardo Borillo <borillo@gmail.com>
2020-03-04 16:07:52 +0100

    Ajustar switch con break y pasar a lambda la iteración entre comandos

--- src/main/java/es/redflag/katas/marsrover/Rover.java
+++ src/main/java/es/redflag/katas/marsrover/Rover.java
@@ -3,81 +3,83 @@ package es.redflag.katas.marsrover;
 import java.util.Arrays;
 import java.util.List;
 
+public class Rover
+{
     private Compass compass;
 
-    public Rover(){
-        compass= new Compass();
+    public Rover()
+    {
+        compass = new Compass();
     }
 
-    public String orientation() {
+    public String orientation()
+    {
         return compass.orientation();
     }
 
-    public void executeOrder(String commands) {
-        for (char command : commands.toCharArray()) {
-            executeCommand(command);
+    public void execute(String commands)
+    {
+        extractCommandsToExecute(commands)
+                .forEach(this::executeCommand);
+    }
+
+    private void executeCommand(String command)
+    {
+        if (command.equals("L"))
+        {
+            turnLeft();
+        }
+        
+        if (command.equals("R"))
+        {
+            turnRight();
         }
     }
 
-    private void executeCommand(char command) {
-        String myCommand = String.valueOf(command);
-        if (myCommand.equals("L")) turnLeft();
-        if (myCommand.equals("R")) turnRight();
-    }
-
-    private void turnRight() {
+    private void turnRight()
+    {
         String orientation = compass.orientation();
-        switch (orientation){
+
+        switch (orientation)
+        {
             case "N":
                 compass.changeDirection("E");
-                return;
+                break;
             case "E":
                 compass.changeDirection("S");
-                return;
+                break;
             case "S":
                 compass.changeDirection("W");
-                return;
+                break;
             case "W":
                 compass.changeDirection("N");
-                return;
-            default:
-                return;
+                break;
         }
     }
 
-    private void turnLeft() {
+    private void turnLeft()
+    {
         String orientation = compass.orientation();
-        switch (orientation){
+
+        switch (orientation)
+        {
             case "N":
                 compass.changeDirection("W");
-                return;
+                break;
             case "E":
                 compass.changeDirection("N");
-                return;
+                break;
             case "S":
                 compass.changeDirection("E");
-                return;
+                break;
             case "W":
                 compass.changeDirection("S");
-                return;
-            default:
-                return;
+                break;
         }
     }
 
-    private class Compass{
-        private String facing;
-
-        public Compass(){
-            facing = "N";
-        }
-
-        public void changeDirection(String orientation){
-            facing = orientation;
-        }
-
-        public String orientation(){
-            return facing;
-        }
+    private List<String> extractCommandsToExecute(String commands)
+    {
+        return Arrays.asList(commands.split(""));
     }
 }
